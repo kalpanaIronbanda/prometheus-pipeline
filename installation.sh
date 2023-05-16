b5347925be1e2b176ca84502a16370aef73a3dda
kalpana <kalpanaironbanda@gmail.com>
2023-05-16 15:51:44 +0530

    added latest version

--- installation.sh
+++ installation.sh
@@ -2,9 +2,9 @@
 yum update -y
 sudo su -
 cd /opt
-wget https://github.com/prometheus/prometheus/releases/download/v2.39.1/prometheus-2.39.1.linux-amd64.tar.gz
-tar -xf prometheus-2.39.1.linux-amd64.tar.gz
-ln -s /opt/prometheus-2.39.1 prometheus
-rm -fr prometheus-2.39.1.linux-amd64.tar.gz
-cd prometheus/
-./prometheus
+wget https://github.com/prometheus/prometheus/releases/download/v2.44.0/prometheus-2.44.0.linux-amd64.tar.gz
+tar -xzvf prometheus-2.44.0.linux-amd64.tar.gz
+rm -rf prometheus-2.44.0.linux-amd64.tar.gz
+ln -s /opt/prometheus-2.44.0.linux-amd64 prometheus
+cd prometheus
+nohup ./prometheus &
